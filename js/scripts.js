ff6d0a5c3e87421a8c6b270793937f4db1a88636
Stewart ilondanga <stilondanga@gmail.com>
2019-02-10 20:49:46 +0300

    add js/scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -13,7 +13,7 @@ console.log('Start');
 window.addEventListener('click', function() {
   console.log('event 1');
 });
-/*window.addEventListener('click', function () {
+window.addEventListener('click', function() {
   debugger;
   // click the ⏵ button to resume the JS code execution
   console.log('event 2');
